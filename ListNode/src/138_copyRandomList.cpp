db87c1437d69ac8f050c6d0991484443d8f2e1ac
letMeEmoForAWhile <279892049@qq.com>
2024-07-28 23:07:05 +0800

    update

--- ListNode/src/138_copyRandomList.cpp
+++ ListNode/src/138_copyRandomList.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <unordered_map>
 // 传统链表节点
 struct ListNode{
     int val;
@@ -18,9 +18,33 @@ public:
     Node(int x) : val(x), next(nullptr), random(nullptr){}
 };
 
+// 双迭代 + 哈希表
 class solution{
 public:
     Node* copyRandomList(Node* head){
+        std::unordered_map<Node*, Node*> map;
+
+        Node* dummy = new Node(-1);
+        Node* tail = dummy;
+
+        if(head == nullptr) return nullptr;
+
+        while(head != nullptr){
+
+            // 按照 next 拷贝
+            Node* new_node = new Node(head->val);
+            tail->next = new_node;
+            tail = tail->next;
+            map[new_node] = head;
+        }
+
+        Node* cur = dummy->next;
+        while(cur != nullptr){
+            cur->random = map[cur]->random;
+            cur = cur->next;
+        }
+
+        return dummy->next;
 
     }
 };
